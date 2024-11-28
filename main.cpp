6d092767d47673bb26767876f7bf8a6d60208b01
WuHongjuan <wuhj@dfcv.com.cn>
2024-11-28 17:21:03 +0800

    实现short函数

--- main.cpp
+++ main.cpp
@@ -3,7 +3,19 @@ using namespace std;
 
 void sort(int aarr[],int size)
 {
-	
+	int tmp = 0;
+	for(int i=0;i<size-1;++i)
+	{
+		for(int j=0;j<size-1-i;++j)
+		{
+			if(arr[j]>arr[j+1])
+			{
+				tmp = arr[j];
+				arr[j]=arr[j-1];
+				arr[j+1]=tmp;
+			}
+		}
+	}
 }
 
 int main ()
