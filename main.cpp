0fa6bf437dd1858bf2aa7aa3bfc5f92c0373f9dd
WuHongjuan <wuhj@dfcv.com.cn>
2024-11-30 14:27:15 +0800

    修改sort算法为快排算法“

--- main.cpp
+++ main.cpp
@@ -1,28 +1,51 @@
 #include <iostream>
 using namespace std;
 
+void sort(int arr[],int i,int j)
+{
+	int val = arr[i];
+	int l = i;
+	int r = j;
+	while(l<r)
+	{
+		while(l<r&&arr[r]>=val)
+		{
+			r--;
+		}
+		if(l<r)
+		{
+			arr[l++]=arr[r]);
+		}
+		while(l<r&&arr[l] <=val)
+		{
+			r++;
+		}
+		if(l<r)
+		{
+			arr[r--]=arr[l]);
+		}
+	}	
+	arr[l] =val;
+	sort(arr,i,l-1);
+	sort(arr,l+1,j);
+	
+}
+
 void sort(int aarr[],int size)
 {
-	int tmp = 0;
-	for(int i=0;i<size-1;++i)
-	{
-		for(int j=0;j<size-1-i;++j)
-		{
-			if(arr[j]>arr[j+1])
-			{
-				tmp = arr[j];
-				arr[j]=arr[j-1];
-				arr[j+1]=tmp;
-			}
-		}
-	}
+	sort(arr[],0, size);
 }
 
 int main ()
-{
+
 	int arr[] = {12,4,89,43,21,78};
 	int size = sizeof(arr)/sizeof(arr[0]);
 	sort(arr,size);
+	for (int i=0;i<size;++i)
+	{
+		cout<<arr[i]<<" ";
+	}
+	cout <<end;
 	return 0;
 	
 }
